0ccd1d8c9c3d1e442195116b79ef47bc58c35918
user name <github tpttyditi123@naver.com>
2021-08-27 14:24:35 +0900

    환경셋팅 완료

---
+++ src/main/java/com/example/blog/blog/BlogApplication.java
@@ -0,0 +1,18 @@
+package com.example.blog.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
+
+
